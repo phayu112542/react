324594cd264d4c1758d953240a49674d069bb952
phayu <kstormmer@gmail.com>
2021-02-17 17:33:47 +0700

    react

---
+++ test/src/Appheader.js
@@ -0,0 +1,11 @@
+function Appheader() {
+    
+return (
+   
+      <header className="App-header">
+          <img className ="header-logo" src ="/pic/logo.png"/>
+      
+      </header>
+      );
+      }
+export default Appheader;
---
+++ test/src/items.js
@@ -0,0 +1,24 @@
+const items =[
+    {
+      title :"man",
+      thumbnaiUrl :"/pic/son.jpg"
+    },
+    {
+      title :"gril",
+      thumbnaiUrl :"/pic/woman.jpeg"
+      
+    
+    },
+    {
+      title :"boy",
+      thumbnaiUrl :"/pic/boy.jpg"
+      
+    
+    },
+    {
+      title :"view",
+      thumbnaiUrl :"/pic/moon.jpg"
+    
+    }
+  ];
+  export default items;
